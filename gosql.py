b3a8ebc359a2fa1f3dc424ec97e153f6c29fc7bf
John Evans <john@milo.com>
2010-09-15 02:12:54 -0700

    Initial 'select' / db_num support.

--- gosql.py
+++ gosql.py
@@ -9,18 +9,19 @@ class Client(object):
         if connection is None:
             connection = psycopg2.connect("dbname=gosql user=gosql")
         self.connection = connection
+        self.db_num = 0
 
     def _do_single_read(self, sql, *args):
         cursor = self.connection.cursor()
         #print "SQL: %s" % cursor.mogrify(sql, args)
-        cursor.execute(sql, args)
+        cursor.execute(sql, (self.db_num,) + args)
         results = cursor.fetchone()
         self.connection.rollback()
         return results[0]
 
     def _do_write(self, sql, *args):
         cursor = self.connection.cursor()
-        cursor.execute(sql, args)
+        cursor.execute(sql, (self.db_num,) + args)
         self.connection.commit()
         cursor.close()
 
@@ -29,47 +30,50 @@ class Client(object):
             return default
         return v
 
+    def select(self, db_num):
+        self.db_num = db_num
+
     def get(self, key, default=None):
-        v = self._do_single_read("SELECT x_get(%s)", key)
+        v = self._do_single_read("SELECT x_get(%s, %s)", key)
         return self._default(v, default)
 
     def set(self, key, value):
-        self._do_write("SELECT x_set(%s, %s)", key, value)
+        self._do_write("SELECT x_set(%s, %s, %s)", key, value)
 
     def delete(self, key):
-        self._do_write("SELECT x_del(%s)", key)
+        self._do_write("SELECT x_del(%s, %s)", key)
 
     def lpush(self, key, element):
-        self._do_write("SELECT x_lpush(%s, %s)", key, element)
+        self._do_write("SELECT x_lpush(%s, %s, %s)", key, element)
 
     def rpush(self, key, element):
-        self.do_write("SELECT x_rpush(%s, %s)", key, element)
+        self.do_write("SELECT x_rpush(%s, %s, %s)", key, element)
 
     def lpop(self, key, default=None):
-        v = self._do_single_read("SELECT x_lpop(%s)", key)
+        v = self._do_single_read("SELECT x_lpop(%s, %s)", key)
         return self._default(v, default)
 
     def rpop(self, key, default=None):
-        v = self._do_single_read("SELECT x_rpop(%s)", key)
+        v = self._do_single_read("SELECT x_rpop(%s, %s)", key)
         return self._default(v, default)
 
     def exists(self, key):
-        v = self._do_single_read("SELECT x_exists(%s)", key)
+        v = self._do_single_read("SELECT x_exists(%s, %s)", key)
         return bool(v)
 
     def rename(self, old_key, new_key):
-        self._do_write("SELECT x_rename(%s, %s)", old_key, new_key)
+        self._do_write("SELECT x_rename(%s, %s, %s)", old_key, new_key)
 
     def renamenx(self, old_key, new_key):
-        self._do_write("SELECT x_renamenx(%s, %s)", old_key, new_key)
+        self._do_write("SELECT x_renamenx(%s, %s, %s)", old_key, new_key)
 
     def dbsize(self):
-        v = self._do_single_read("SELECT x_dbsize()")
+        v = self._do_single_read("SELECT x_dbsize(%s)")
         return v
 
     def mget(self, keys):
-        v = self._do_single_read("SELECT x_mget(%s)", keys)
+        v = self._do_single_read("SELECT x_mget(%s, %s)", keys)
         return v
 
     def expire(self, key, timestamp):
-        self._do_write("SELECT x_expire(%s, %s)", key, timestamp)
+        self._do_write("SELECT x_expire(%s, %s, %s)", key, timestamp)
--- install.sql
+++ install.sql
@@ -2,24 +2,31 @@ BEGIN;
 
 DROP TABLE IF EXISTS kvs;
 DROP FUNCTION IF EXISTS expired(ts TIMESTAMP WITHOUT TIME ZONE);
-DROP FUNCTION IF EXISTS x_exists(in_k TEXT);
-DROP FUNCTION IF EXISTS x_get(in_k TEXT);
-DROP FUNCTION IF EXISTS x_set(in_k TEXT, in_v TEXT);
-DROP FUNCTION IF EXISTS x_del(in_k TEXT);
-DROP FUNCTION IF EXISTS x_lpush(in_k TEXT, in_v TEXT);
-DROP FUNCTION IF EXISTS x_rpush(in_k TEXT, in_v TEXT);
-DROP FUNCTION IF EXISTS x_lpop(in_k TEXT);
-DROP FUNCTION IF EXISTS x_rpop(in_k TEXT);
-DROP FUNCTION IF EXISTS x_rename(old_k TEXT, new_k TEXT);
-DROP FUNCTION IF EXISTS x_renamenx(old_k TEXT, new_k TEXT);
-DROP FUNCTION IF EXISTS x_dbsize();
-DROP FUNCTION IF EXISTS x_mget(in_ks TEXT[]);
+DROP FUNCTION IF EXISTS get_row_for_key(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_exists(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_get(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_set(in_n INTEGER, in_k TEXT, in_v TEXT);
+DROP FUNCTION IF EXISTS x_del(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_lpush(in_n INTEGER, in_k TEXT, in_v TEXT);
+DROP FUNCTION IF EXISTS x_rpush(in_n INTEGER, in_k TEXT, in_v TEXT);
+DROP FUNCTION IF EXISTS x_lpop(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_rpop(in_n INTEGER, in_k TEXT);
+DROP FUNCTION IF EXISTS x_rename(in_n INTEGER, in_old_k TEXT,
+                                 in_new_k TEXT);
+DROP FUNCTION IF EXISTS x_renamenx(in_n INTEGER, in_old_k TEXT,
+                                   in_new_k TEXT);
+DROP FUNCTION IF EXISTS x_dbsize(in_n INTEGER);
+DROP FUNCTION IF EXISTS x_mget(in_n INTEGER, in_ks TEXT[]);
+DROP FUNCTION IF EXISTS x_expire(in_n INTEGER, in_k TEXT,
+                                 in_use_by TIMESTAMP WITHOUT TIME ZONE);
 
 CREATE TABLE kvs (
-    k TEXT PRIMARY KEY,
+    n INTEGER,
+    k TEXT,
     v TEXT,
     a TEXT[],
     use_by TIMESTAMP WITHOUT TIME ZONE,
+    PRIMARY KEY(n, k),
     CHECK(v IS NOT NULL OR a IS NOT NULL)
 );
 
@@ -33,181 +40,177 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_exists(in_k TEXT) RETURNS BOOLEAN AS $$
-DECLARE
-    c INTEGER;
-BEGIN
-    SELECT COUNT(*)
-    INTO c
-    FROM kvs
-    WHERE k = in_k;
-    RETURN c > 0;
-END
-$$ LANGUAGE plpgsql;
-
-CREATE FUNCTION x_get(in_k TEXT) RETURNS TEXT AS $$
+CREATE FUNCTION get_row_for_key(in_n INTEGER, in_k TEXT)
+     RETURNS record AS $$
 DECLARE
     t_row kvs%ROWTYPE;
 BEGIN
     SELECT *
     INTO t_row
     FROM kvs
-    WHERE k = in_k
+    WHERE n = in_n
+    AND k = in_k
     AND NOT expired(use_by);
+    RETURN t_row;
+END
+$$ LANGUAGE plpgsql;
 
+CREATE FUNCTION x_exists(in_n INTEGER, in_k TEXT) RETURNS BOOLEAN AS $$
+DECLARE
+    c INTEGER;
+BEGIN
+    SELECT COUNT(*)
+    INTO c
+    FROM kvs
+    WHERE n = in_n
+    AND k = in_k;
+    RETURN c > 0;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION x_get(in_n INTEGER, in_k TEXT) RETURNS TEXT AS $$
+DECLARE
+    t_row kvs%ROWTYPE;
+BEGIN
+    t_row := get_row_for_key(in_n, in_k);
     RETURN t_row.v;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_set(in_k TEXT, in_v TEXT) RETURNS VOID AS $$
+CREATE FUNCTION x_set(in_n INTEGER, in_k TEXT, in_v TEXT)
+    RETURNS VOID AS $$
 DECLARE
     c INTEGER;
 BEGIN
--- Lame to do the query every time, but for now...
-    SELECT COUNT(k)
-    INTO c
-    FROM kvs
-    WHERE k = in_k;
-    IF c > 0 THEN
+    IF x_exists(in_n, in_k) THEN
        UPDATE kvs
        SET v = in_v,
            use_by = NULL
-       WHERE k = in_k;
+       WHERE n = in_n
+       AND k = in_k;
     ELSE
-       INSERT INTO kvs (k, v) VALUES (in_k, in_v);
+       INSERT INTO kvs (n, k, v)
+       VALUES (in_n, in_k, in_v);
     END IF;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_del(in_k TEXT) RETURNS VOID AS $$
+CREATE FUNCTION x_del(in_n INTEGER, in_k TEXT) RETURNS VOID AS $$
 BEGIN
     DELETE FROM kvs
-    WHERE k = in_k;
+    WHERE n = in_n
+    AND k = in_k;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_lpush(in_k TEXT, in_v TEXT) RETURNS VOID AS $$
+CREATE FUNCTION x_lpush(in_n INTEGER, in_k TEXT, in_v TEXT)
+    RETURNS VOID AS $$
 DECLARE
     c INTEGER;
 BEGIN
--- Lame to do the query every time, but for now...
-    SELECT COUNT(k)
-    INTO c
-    FROM kvs
-    WHERE k = in_k;
-    IF c > 0 THEN
+    IF x_exists(in_n, in_k) THEN
        UPDATE kvs
        SET a = array[in_v] || CASE
                    WHEN (a IS NULL) THEN array[]::text[]
                    ELSE a
                END,
            v = NULL
-       WHERE k = in_k;
+       WHERE n = in_n
+       AND k = in_k;
     ELSE
-       INSERT INTO kvs (k, a) VALUES (in_k, array[in_v]);
+       INSERT INTO kvs (n, k, a) VALUES (in_n, in_k, array[in_v]);
     END IF;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_rpush(in_k TEXT, in_v TEXT) RETURNS VOID AS $$
+CREATE FUNCTION x_rpush(in_n INTEGER, in_k TEXT, in_v TEXT) RETURNS VOID AS $$
 DECLARE
     c INTEGER;
 BEGIN
--- Lame to do the query every time, but for now...
-    SELECT COUNT(k)
-    INTO c
-    FROM kvs
-    WHERE k = in_k;
-    IF c > 0 THEN
+    IF x_exists(in_n, in_k) THEN
        UPDATE kvs
        SET a = CASE
                    WHEN (a IS NULL) THEN array[]::text[]
                    ELSE a
                END || array[in_v],
            v = NULL
-       WHERE k = in_k;
+       WHERE n = in_n
+       AND k = in_k;
     ELSE
-       INSERT INTO kvs (k, a) VALUES (in_k, array[in_v]);
+       INSERT INTO kvs (n, k, a) VALUES (in_n, in_k, array[in_v]);
     END IF;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_lpop(in_k TEXT) RETURNS TEXT AS $$
+CREATE FUNCTION x_lpop(in_n INTEGER, in_k TEXT) RETURNS TEXT AS $$
 DECLARE
     t_row kvs%ROWTYPE;
     a_len INTEGER;
 BEGIN
-    SELECT *
-    INTO t_row
-    FROM kvs
-    WHERE k = in_k
-    AND NOT expired(use_by);
+    t_row := get_row_for_key(in_n, in_k);
 
     a_len := array_length(t_row.a, 1);
 
     IF a_len > 1 THEN
         UPDATE kvs
         SET a = a[2:a_len]
-        WHERE k = in_k;
+        WHERE n = in_n
+        AND k = in_k;
     ELSE
         DELETE FROM kvs
-        WHERE k = in_k;
+        WHERE n = in_n
+        AND k = in_k;
     END IF;
 
     RETURN t_row.a[1];
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_rpop(in_k TEXT) RETURNS TEXT AS $$
+CREATE FUNCTION x_rpop(in_n INTEGER, in_k TEXT) RETURNS TEXT AS $$
 DECLARE
     t_row kvs%ROWTYPE;
     a_len INTEGER;
 BEGIN
-    SELECT *
-    INTO t_row
-    FROM kvs
-    WHERE k = in_k
-    AND NOT expired(use_by);
-
+    t_row := get_row_for_key(in_n, in_k);
     a_len := array_length(t_row.a, 1);
 
     IF a_len > 1 THEN
         UPDATE kvs
         SET a = a[1:a_len - 1]
-        WHERE k = in_k;
+        WHERE n = in_n
+        AND k = in_k;
     ELSE
         DELETE FROM kvs
-        WHERE k = in_k;
+        WHERE n = in_n
+        AND k = in_k;
     END IF;
 
     RETURN t_row.a[a_len];
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_rename(old_k TEXT, new_k TEXT) RETURNS VOID AS $$
+CREATE FUNCTION x_rename(in_n INTEGER, in_old_k TEXT, in_new_k TEXT)
+    RETURNS VOID AS $$
 BEGIN
-    DELETE FROM kvs
-    WHERE k = new_k;
+    PERFORM x_del(in_n, in_new_k);
 
     UPDATE kvs
-    SET k = new_k
-    WHERE k = old_k;
+    SET k = in_new_k
+    WHERE n = in_n
+    AND k = in_old_k;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_renamenx(old_k TEXT, new_k TEXT) RETURNS BOOLEAN AS $$
+CREATE FUNCTION x_renamenx(in_n INTEGER, in_old_k TEXT, in_new_k TEXT)
+    RETURNS BOOLEAN AS $$
 DECLARE
     c INTEGER;
 BEGIN
-    SELECT COUNT(*)
-    INTO c
-    FROM kvs
-    WHERE k = new_k;
-
-    IF c <= 0 THEN
+    IF NOT x_exists(in_n, in_new_k) THEN
         UPDATE kvs
-        SET k = new_k
-        WHERE k = old_k;
+        SET k = in_new_k
+        WHERE n = in_n
+        AND k = in_old_k;
         RETURN True;
     ELSE
         RETURN False;
@@ -215,16 +218,19 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_dbsize() RETURNS INTEGER AS $$
+CREATE FUNCTION x_dbsize(in_n INTEGER) RETURNS INTEGER AS $$
 DECLARE
     c INTEGER;
 BEGIN
-    SELECT COUNT(*) INTO c FROM kvs;
+    SELECT COUNT(*)
+    INTO c
+    FROM kvs
+    WHERE n = in_n;
     RETURN c;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_mget(in_ks TEXT[]) RETURNS TEXT[] AS $$
+CREATE FUNCTION x_mget(in_n INTEGER, in_ks TEXT[]) RETURNS TEXT[] AS $$
 DECLARE
     t_row kvs%ROWTYPE;
     res TEXT[];
@@ -235,7 +241,8 @@ BEGIN
     res := array_fill(NULL::TEXT, ARRAY[len]);
     FOR t_row IN SELECT *
                  FROM kvs
-                 WHERE in_ks @> ARRAY[k]
+                 WHERE n = in_n
+                 AND in_ks @> ARRAY[k]
                  AND NOT expired(use_by)
     LOOP
         FOR idx IN 1 .. len
@@ -249,12 +256,14 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION x_expire(in_k TEXT, in_use_by TIMESTAMP WITHOUT TIME ZONE)
+CREATE FUNCTION x_expire(in_n INTEGER, in_k TEXT,
+                         in_use_by TIMESTAMP WITHOUT TIME ZONE)
      RETURNS VOID AS $$
 BEGIN
     UPDATE kvs
     SET use_by = in_use_by
-    WHERE k = in_k;
+    WHERE n = in_n
+    AND k = in_k;
 END
 $$ LANGUAGE plpgsql;
 
@@ -272,167 +281,168 @@ CREATE TABLE tests (
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
+SELECT x_set(0, 'a', 'b');
 
 INSERT INTO tests (description, test_result)
-VALUES ('existence check for existent key', x_exists('a') = True);
+VALUES ('existence check for existent key', x_exists(0, 'a') = True);
 
 -------------------
 TRUNCATE TABLE kvs;
 
 INSERT INTO tests (description, test_result)
-VALUES ('existence check for non-existent key', x_exists('abc') = False);
+VALUES ('existence check for non-existent key', x_exists(0, 'abc') = False);
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
+SELECT x_set(0, 'a', 'b');
 
 INSERT INTO tests (description, test_result)
-VALUES ('getting a valid key', x_get('a') = 'b');
+VALUES ('getting a valid key', x_get(0, 'a') = 'b');
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
-SELECT x_del('a');
+SELECT x_set(0, 'a', 'b');
+SELECT x_del(0, 'a');
 
 INSERT INTO tests (description, test_result)
-VALUES ('deleting an existent key', x_get('a') IS NULL);
-
--------------------
-TRUNCATE TABLE kvs;
-
-INSERT INTO tests (description, test_result)
-VALUES ('getting an invalid key', x_get('c') IS NULL);
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_lpush('r', 'foo');
-SELECT x_rpush('r', 'bar');
-
-INSERT INTO tests (description, test_result)
-VALUES ('lpop once', x_lpop('r') = 'foo');
-
-INSERT INTO tests (description, test_result)
-VALUES ('lpop twice', x_lpop('r') = 'bar');
-
-INSERT INTO tests (description, test_result)
-VALUES ('lpop thrice', x_lpop('r') IS NULL);
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_lpush('r', 'foo');
-SELECT x_rpush('r', 'bar');
-
-INSERT INTO tests (description, test_result)
-VALUES ('rpop once', x_rpop('r') = 'bar');
-
-INSERT INTO tests (description, test_result)
-VALUES ('rpop twice', x_rpop('r') = 'foo');
-
-INSERT INTO tests (description, test_result)
-VALUES ('rpop thrice', x_rpop('r') IS NULL);
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_set('a', 'b');
-SELECT x_rename('a', 'q');
-
-INSERT INTO tests (description, test_result)
-VALUES ('rename to non-existent key', x_get('q') = 'b');
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_set('aa', 'bb');
-SELECT x_set('bb', 'cc');
-
-SELECT x_rename('aa', 'bb');
-
-INSERT INTO tests (description, test_result)
-VALUES ('rename to existent key', x_get('bb') = 'bb');
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_set('a', 'b');
-SELECT x_renamenx('a', 'q');
-
-INSERT INTO tests (description, test_result)
-VALUES ('renamenx to non-existent key', x_get('q') = 'b');
-
--------------------
-TRUNCATE TABLE kvs;
-
-SELECT x_set('aa', 'bb');
-SELECT x_set('bb', 'cc');
-
-SELECT x_renamenx('aa', 'bb');
-
-INSERT INTO tests (description, test_result)
-VALUES ('renamenx to existent key leaves original', x_get('aa') = 'bb');
-
-INSERT INTO tests (description, test_result)
-VALUES ('renamenx to existent key leaves destination', x_get('bb') = 'cc');
+VALUES ('deleting an existent key', x_get(0, 'a') IS NULL);
 
 -------------------
 TRUNCATE TABLE kvs;
 
 INSERT INTO tests (description, test_result)
-VALUES ('dbsize on empty db', x_dbsize() = 0);
-
-SELECT x_set('a', 'b');
-SELECT x_set('c', 'd');
-SELECT x_set('e', 'f');
-
-INSERT INTO tests (description, test_result)
-VALUES ('dbsize on non-empty db', x_dbsize() = 3);
+VALUES ('getting an invalid key', x_get(0, 'c') IS NULL);
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
-SELECT x_set('c', 'd');
+SELECT x_lpush(0, 'r', 'foo');
+SELECT x_rpush(0, 'r', 'bar');
+
+INSERT INTO tests (description, test_result)
+VALUES ('lpop once', x_lpop(0, 'r') = 'foo');
+
+INSERT INTO tests (description, test_result)
+VALUES ('lpop twice', x_lpop(0, 'r') = 'bar');
+
+INSERT INTO tests (description, test_result)
+VALUES ('lpop thrice', x_lpop(0, 'r') IS NULL);
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_lpush(0, 'r', 'foo');
+SELECT x_rpush(0, 'r', 'bar');
+
+INSERT INTO tests (description, test_result)
+VALUES ('rpop once', x_rpop(0, 'r') = 'bar');
+
+INSERT INTO tests (description, test_result)
+VALUES ('rpop twice', x_rpop(0, 'r') = 'foo');
+
+INSERT INTO tests (description, test_result)
+VALUES ('rpop thrice', x_rpop(0, 'r') IS NULL);
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_set(0, 'a', 'b');
+SELECT x_rename(0, 'a', 'q');
+
+INSERT INTO tests (description, test_result)
+VALUES ('rename to non-existent key', x_get(0, 'q') = 'b');
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_set(0, 'aa', 'bb');
+SELECT x_set(0, 'bb', 'cc');
+
+SELECT x_rename(0, 'aa', 'bb');
+
+INSERT INTO tests (description, test_result)
+VALUES ('rename to existent key', x_get(0, 'bb') = 'bb');
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_set(0, 'a', 'b');
+SELECT x_renamenx(0, 'a', 'q');
+
+INSERT INTO tests (description, test_result)
+VALUES ('renamenx to non-existent key', x_get(0, 'q') = 'b');
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_set(0, 'aa', 'bb');
+SELECT x_set(0, 'bb', 'cc');
+
+SELECT x_renamenx(0, 'aa', 'bb');
+
+INSERT INTO tests (description, test_result)
+VALUES ('renamenx to existent key leaves original', x_get(0, 'aa') = 'bb');
+
+INSERT INTO tests (description, test_result)
+VALUES ('renamenx to existent key leaves destination', x_get(0, 'bb') = 'cc');
+
+-------------------
+TRUNCATE TABLE kvs;
+
+INSERT INTO tests (description, test_result)
+VALUES ('dbsize on empty db', x_dbsize(0) = 0);
+
+SELECT x_set(0, 'a', 'b');
+SELECT x_set(0, 'c', 'd');
+SELECT x_set(0, 'e', 'f');
+
+INSERT INTO tests (description, test_result)
+VALUES ('dbsize on non-empty db', x_dbsize(0) = 3);
+
+-------------------
+TRUNCATE TABLE kvs;
+
+SELECT x_set(0, 'a', 'b');
+SELECT x_set(0, 'c', 'd');
 
 INSERT INTO tests (description, test_result)
 VALUES ('mget on multiple existent keys',
-        x_mget(array['a', 'c']::TEXT[]) = array['b', 'd']::TEXT[]);
+        x_mget(0, array['a', 'c']::TEXT[]) = array['b', 'd']::TEXT[]);
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
-SELECT x_set('c', 'd');
+SELECT x_set(0, 'a', 'b');
+SELECT x_set(0, 'c', 'd');
 
 INSERT INTO tests (description, test_result)
 VALUES ('mget on mixed existent/non-existent keys',
-        x_mget(array['a', 'x', 'c']::TEXT[]) = array['b', NULL, 'd']::TEXT[]);
+        x_mget(0, array['a', 'x', 'c']::TEXT[]) =
+                  array['b', NULL, 'd']::TEXT[]);
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
+SELECT x_set(0, 'a', 'b');
 -- Warning, this test will become obselete in August 2169.
-SELECT x_expire('a'::TEXT, TIMESTAMP '2169-08-15 12:34:56');
+SELECT x_expire(0, 'a'::TEXT, TIMESTAMP '2169-08-15 12:34:56');
 
 INSERT INTO tests (description, test_result)
-VALUES ('can fetch non-expired result', x_get('a') = 'b');
+VALUES ('can fetch non-expired result', x_get(0, 'a') = 'b');
 
 -------------------
 TRUNCATE TABLE kvs;
 
-SELECT x_set('a', 'b');
+SELECT x_set(0, 'a', 'b');
 -- Warning, this test will become obselete in August 2169.
-SELECT x_expire('a'::TEXT, TIMESTAMP '2008-08-15 12:34:56');
+SELECT x_expire(0, 'a'::TEXT, TIMESTAMP '2008-08-15 12:34:56');
 
 INSERT INTO tests (description, test_result)
-VALUES ('can NOT fetch expired result', x_get('a') IS NULL);
+VALUES ('can NOT fetch expired result', x_get(0, 'a') IS NULL);
 
-
+-- TODO: Tests with different databases
 
 --------------------------------------------------------------------
 --------------------------------------------------------------------
